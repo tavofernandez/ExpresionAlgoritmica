2b0987e02cb19a82270dfed4d0d1acf23a2d87ab
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-22 21:05:29 -0500

    Modificamos los nombres de los principales controles para su mejor entendimiento

--- frmEcuacion.Designer.cs
+++ frmEcuacion.Designer.cs
@@ -36,11 +36,11 @@ namespace ExpresionAlgoritmica
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -50,7 +50,7 @@ namespace ExpresionAlgoritmica
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(800, 49);
+            this.label1.Size = new System.Drawing.Size(1082, 78);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enunciado:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -59,9 +59,9 @@ namespace ExpresionAlgoritmica
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.pictureBox1.Image = global::ExpresionAlgoritmica.Properties.Resources.Ejercicio1;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 49);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 78);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 188);
+            this.pictureBox1.Size = new System.Drawing.Size(1082, 218);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -70,9 +70,9 @@ namespace ExpresionAlgoritmica
             // 
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(0, 237);
+            this.label2.Location = new System.Drawing.Point(0, 296);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(800, 49);
+            this.label2.Size = new System.Drawing.Size(1082, 65);
             this.label2.TabIndex = 2;
             this.label2.Text = "Solucion:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -82,7 +82,7 @@ namespace ExpresionAlgoritmica
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(157, 310);
+            this.label3.Location = new System.Drawing.Point(479, 444);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 28);
             this.label3.TabIndex = 3;
@@ -92,7 +92,7 @@ namespace ExpresionAlgoritmica
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(156, 352);
+            this.label4.Location = new System.Drawing.Point(478, 487);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(30, 28);
             this.label4.TabIndex = 4;
@@ -102,7 +102,7 @@ namespace ExpresionAlgoritmica
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(156, 390);
+            this.label5.Location = new System.Drawing.Point(478, 525);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(28, 28);
             this.label5.TabIndex = 5;
@@ -112,63 +112,63 @@ namespace ExpresionAlgoritmica
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(68, 456);
+            this.label6.Location = new System.Drawing.Point(390, 591);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(118, 28);
             this.label6.TabIndex = 6;
             this.label6.Text = "Resultado:";
             // 
-            // label7
+            // lblresultado
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(217, 456);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(39, 28);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "---";
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(539, 591);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(39, 28);
+            this.lblresultado.TabIndex = 7;
+            this.lblresultado.Text = "---";
             // 
-            // textBox1
+            // txta
             // 
-            this.textBox1.Location = new System.Drawing.Point(222, 318);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 8;
+            this.txta.Location = new System.Drawing.Point(544, 452);
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.TabIndex = 8;
             // 
-            // textBox2
+            // txtb
             // 
-            this.textBox2.Location = new System.Drawing.Point(222, 359);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 9;
+            this.txtb.Location = new System.Drawing.Point(544, 494);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.TabIndex = 9;
             // 
-            // textBox3
+            // txtc
             // 
-            this.textBox3.Location = new System.Drawing.Point(222, 398);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 10;
+            this.txtc.Location = new System.Drawing.Point(544, 533);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.TabIndex = 10;
             // 
-            // button1
+            // btncalcular
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(385, 339);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 40);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Calcular";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(707, 474);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(119, 40);
+            this.btncalcular.TabIndex = 11;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
             // 
             // frmEcuacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 553);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label7);
+            this.ClientSize = new System.Drawing.Size(1082, 693);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.lblresultado);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -178,6 +178,7 @@ namespace ExpresionAlgoritmica
             this.Controls.Add(this.label1);
             this.Name = "frmEcuacion";
             this.Text = "frmEcuacion";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -193,10 +194,10 @@ namespace ExpresionAlgoritmica
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.Button btncalcular;
     }
 }
