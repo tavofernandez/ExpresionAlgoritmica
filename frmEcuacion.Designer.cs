40359349fb504f04f77cdb1f31038ada2f78e598
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-28 23:26:34 -0500

    Mejoramos la interfaz de usuario agregando un fondo para el formulario y modificando el tipo de letra y color de los controles

--- frmEcuacion.Designer.cs
+++ frmEcuacion.Designer.cs
@@ -30,7 +30,6 @@ namespace ExpresionAlgoritmica
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -41,13 +40,16 @@ namespace ExpresionAlgoritmica
             this.txtb = new System.Windows.Forms.TextBox();
             this.txtc = new System.Windows.Forms.TextBox();
             this.btncalcular = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(1082, 78);
@@ -55,21 +57,12 @@ namespace ExpresionAlgoritmica
             this.label1.Text = "Enunciado:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox1.Image = global::ExpresionAlgoritmica.Properties.Resources.Ejercicio1;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 78);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1082, 218);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // label2
             // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
             this.label2.Location = new System.Drawing.Point(0, 296);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(1082, 65);
@@ -81,89 +74,128 @@ namespace ExpresionAlgoritmica
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(479, 444);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(465, 402);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 28);
+            this.label3.Size = new System.Drawing.Size(39, 36);
             this.label3.TabIndex = 3;
             this.label3.Text = "a:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(478, 487);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(465, 462);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(30, 28);
+            this.label4.Size = new System.Drawing.Size(41, 36);
             this.label4.TabIndex = 4;
             this.label4.Text = "b:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(478, 525);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(465, 513);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(28, 28);
+            this.label5.Size = new System.Drawing.Size(37, 36);
             this.label5.TabIndex = 5;
             this.label5.Text = "c:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(390, 591);
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Aqua;
+            this.label6.Location = new System.Drawing.Point(346, 602);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 28);
+            this.label6.Size = new System.Drawing.Size(160, 36);
             this.label6.TabIndex = 6;
             this.label6.Text = "Resultado:";
             // 
             // lblresultado
             // 
             this.lblresultado.AutoSize = true;
-            this.lblresultado.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblresultado.Location = new System.Drawing.Point(539, 591);
+            this.lblresultado.BackColor = System.Drawing.Color.Transparent;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft YaHei", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.ForeColor = System.Drawing.Color.Aqua;
+            this.lblresultado.Location = new System.Drawing.Point(529, 602);
             this.lblresultado.Name = "lblresultado";
-            this.lblresultado.Size = new System.Drawing.Size(39, 28);
+            this.lblresultado.Size = new System.Drawing.Size(51, 36);
             this.lblresultado.TabIndex = 7;
             this.lblresultado.Text = "---";
             // 
             // txta
             // 
-            this.txta.Location = new System.Drawing.Point(544, 452);
+            this.txta.BackColor = System.Drawing.Color.Fuchsia;
+            this.txta.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txta.ForeColor = System.Drawing.Color.White;
+            this.txta.Location = new System.Drawing.Point(535, 398);
             this.txta.Name = "txta";
-            this.txta.Size = new System.Drawing.Size(100, 20);
+            this.txta.Size = new System.Drawing.Size(100, 35);
             this.txta.TabIndex = 8;
+            this.txta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtb
             // 
-            this.txtb.Location = new System.Drawing.Point(544, 494);
+            this.txtb.BackColor = System.Drawing.Color.Fuchsia;
+            this.txtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtb.ForeColor = System.Drawing.Color.White;
+            this.txtb.Location = new System.Drawing.Point(535, 458);
             this.txtb.Name = "txtb";
-            this.txtb.Size = new System.Drawing.Size(100, 20);
+            this.txtb.Size = new System.Drawing.Size(100, 35);
             this.txtb.TabIndex = 9;
+            this.txtb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtc
             // 
-            this.txtc.Location = new System.Drawing.Point(544, 533);
+            this.txtc.BackColor = System.Drawing.Color.Fuchsia;
+            this.txtc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtc.ForeColor = System.Drawing.Color.White;
+            this.txtc.Location = new System.Drawing.Point(535, 513);
             this.txtc.Name = "txtc";
-            this.txtc.Size = new System.Drawing.Size(100, 20);
+            this.txtc.Size = new System.Drawing.Size(100, 35);
             this.txtc.TabIndex = 10;
+            this.txtc.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btncalcular
             // 
-            this.btncalcular.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btncalcular.Location = new System.Drawing.Point(707, 474);
+            this.btncalcular.BackColor = System.Drawing.Color.Indigo;
+            this.btncalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.ForeColor = System.Drawing.Color.Red;
+            this.btncalcular.Location = new System.Drawing.Point(686, 456);
             this.btncalcular.Name = "btncalcular";
-            this.btncalcular.Size = new System.Drawing.Size(119, 40);
+            this.btncalcular.Size = new System.Drawing.Size(167, 53);
             this.btncalcular.TabIndex = 11;
             this.btncalcular.Text = "Calcular";
-            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.UseVisualStyleBackColor = false;
             this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::ExpresionAlgoritmica.Properties.Resources.Ejercicio1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1082, 218);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // frmEcuacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ExpresionAlgoritmica.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1082, 693);
             this.Controls.Add(this.btncalcular);
             this.Controls.Add(this.txtc);
@@ -177,6 +209,7 @@ namespace ExpresionAlgoritmica
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.Name = "frmEcuacion";
             this.Text = "frmEcuacion";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
